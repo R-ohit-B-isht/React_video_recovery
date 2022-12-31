477912b8908bb370d85242e41162bab1fa3b94a8
Rohit Bisht <rbtunes0@gmail.com>
2022-12-31 22:56:50 +0530

    react convert

--- src/App.js
+++ src/App.js
@@ -1,5 +1,77 @@
-var sudo = require("sudo-prompt");
-const { resolve } = require("path");
+// import { useState, useMemo } from 'react'
+// import { FilesViewer } from './FilesViewer'
+
+// const fs = window.require('fs')
+// const pathModule = window.require('path')
+
+// const { app } = window.require('@electron/remote')
+
+// const formatSize = size => {
+//   var i = Math.floor(Math.log(size) / Math.log(1024))
+//   return (
+//     (size / Math.pow(1024, i)).toFixed(2) * 1 +
+//     ' ' +
+//     ['B', 'kB', 'MB', 'GB', 'TB'][i]
+//   )
+// }
+
+// function App() {
+//   const [path, setPath] = useState(app.getAppPath())
+
+//   const files = useMemo(
+//     () =>
+//       fs
+//         .readdirSync(path)
+//         .map(file => {
+//           const stats = fs.statSync(pathModule.join(path, file))
+//           return {
+//             name: file,
+//             size: stats.isFile() ? formatSize(stats.size ?? 0) : null,
+//             directory: stats.isDirectory()
+//           }
+//         })
+//         .sort((a, b) => {
+//           if (a.directory === b.directory) {
+//             return a.name.localeCompare(b.name)
+//           }
+//           return a.directory ? -1 : 1
+//         }),
+//     [path]
+//   )
+
+//   const onBack = () => setPath(pathModule.dirname(path))
+//   const onOpen = folder => setPath(pathModule.join(path, folder))
+
+//   const [searchString, setSearchString] = useState('')
+//   const filteredFiles = files.filter(s => s.name.startsWith(searchString))
+
+//   return (
+//     <div className="container mt-2">
+//       <h4>{path}</h4>
+//       <div className="form-group mt-4 mb-2">
+//         <input
+//           value={searchString}
+//           onChange={event => setSearchString(event.target.value)}
+//           className="form-control form-control-sm"
+//           placeholder="File search"
+//         />
+//       </div>
+//       <FilesViewer files={filteredFiles} onBack={onBack} onOpen={onOpen} />
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+import { useState, useMemo } from 'react'
+
+const fs = window.require('fs')
+const pathModule = window.require('path')
+
+const { app } = window.require('@electron/remote')
+
+const { resolve } = window.require("path");
 
 // const path = require('path');
 // require("electron-reload")(__dirname);
@@ -11,15 +83,14 @@ const tmp_frames = resolve("./model/tmp_frames/");
 const tmp_framesw = tmp_frames.split("\\").join("\\\\");
 const out_frames = resolve("./model/out_frames/");
 const out_framesw = out_frames.split("\\").join("\\\\");
-const exec = require("child_process").exec;
-const fs = require("fs");
+const exec = window.require("child_process").exec;
 
-const child_process = require("child_process");
+
 var options = {
   name: "Electron",
   icns: "/Applications/Electron.app/Contents/Resources/Electron.icns", // (optional)
 };
-const { ipcRenderer, contextBridge } = require("electron");
+const { ipcRenderer, contextBridge } = window.require("electron");
 
 const WINDOW_API = {
   GetVersion: () => ipcRenderer.invoke("get/version"),
@@ -157,7 +228,6 @@ function empty_my_tmp() {
 }
 function empty_my_out() {
   return new Promise((resolve, reject) => {
-    emptying_out_copy = 0;
     fs.readdir(out_framesw, function (err, files) {
       if (err) {
         throw err;
@@ -170,43 +240,79 @@ function empty_my_out() {
     });
   });
 }
-function generate_tmp_frames(inp) {
-  return new Promise((resolve, reject) => {
-    WINDOW_API.Ffmpeg(inp);
-  });
+
+
+// contextBridge.exposeInMainWorld("api", WINDOW_API);
+
+	let realesr;
+	// let fileInput="C:/Users/rbtun/Videos/V/realesrgan-ncnn-vulkan-20220424-windows/input.jpg";
+	var fileInput=undefined;
+	let fileOutput="C:/Users/rbtun/Videos/V/realesrgan-ncnn-vulkan-20220424-windows/output.png";
+	let tmpFiles=0;
+
+	
+	const apply_realesr=()=>{
+		let esrw=fileInput.split('\\').join('/')
+		let out=esrw.substring(0,esrw.lastIndexOf("/")+1)+"output.png"
+		console.log(out)
+		 return realesr= WINDOW_API.Realesrgan(esrw,out)
+		 console.log(realesr)
+	}
+
+	function call_apply_realesr(){
+	let  x;
+	x=	apply_realesr()
+	if(x==undefined)
+		console.log("undefined")
+	else
+		console.log("done")
+	}
+	const apply_realesr_on_video=async()=>{
+		let esrw=fileInput.split('\\').join('/')
+		let out=esrw.substring(0,esrw.lastIndexOf("/")+1)+"output_Video.mp4"
+		console.log(out)
+		realesr= await WINDOW_API.Realesrgan_on_Video(esrw,out)
+		files_in_temp()
+	}
+
+  const onDrop = files => {
+	fileInput=files[0].path;
+	alert(fileInput)
+} 
+
+async function clear_fileInput(){
+	fileInput=""
+	await WINDOW_API.Clean()
+	files_in_temp()
 }
-function rersgan_tmp_frames() {
-  return new Promise((resolve, reject) => {
-    WINDOW_API.Realesrgan_frames();
-  });
+function get_fileInput(){
+	return fileInput
 }
-function merge_out_frames(inp, out) {
-  return new Promise((resolve, reject) => {
-    WINDOW_API.MergeFrames(inp, out);
-  });
+function files_in_temp(){
+	tmpFiles= WINDOW_API.FilesInTemp()
+	console.log(tmpFiles)
+	
 }
-function finalone(inp, out) {
-  fs.readdir(tmp_framesw, function (err, files) {
-    if (err) {
-      throw err;
-    } else {
-      if (files.length > 0) {
-        WINDOW_API.empty_tmp_frames();
-      }
-    }
-  });
-  fs.readdir(tmp_framesw, function (err, files) {
-    if (err) {
-      throw err;
-    } else {
-      if (files.length > 0) {
-        WINDOW_API.empty_out_frames();
-      }
-    }
-  });
-  WINDOW_API.Ffmpeg(inp);
-  WINDOW_API.Realesrgan_frames();
-  WINDOW_API.MergeFrames(inp, out);
+function files_in_out(){
+	return WINDOW_API.FilesInOut()
 }
 
-contextBridge.exposeInMainWorld("api", WINDOW_API);
+
+
+
+function App() {
+  const[version,setVersion]=useState("");
+  const get_version=async()=>{
+		setVersion(await WINDOW_API.GetVersion())
+	}
+    return (
+      <div>
+<h1>hi{version}</h1>
+<button class="ui button" onClick={get_version}>click me</button>
+      </div>
+      
+  
+  )
+}
+
+export default App
